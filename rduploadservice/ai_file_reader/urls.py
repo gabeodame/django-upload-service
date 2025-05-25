d88e078dd9bd48d0b88d45e68510b87df96b8b10
Gabriel Odame <gabriel.odame@anchordiv.com>
2025-05-24 23:13:16 -0400

    refactor from nextjs tools for text extraction to using langchain

--- rduploadservice/ai_file_reader/urls.py
+++ rduploadservice/ai_file_reader/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import process_files
+from .tasks.task import process_files
+from .tasks.task_status_view import task_status_view
 
 urlpatterns = [
     path('process-files/', process_files, name='process_files'),
+    path('api/tasks/<uuid:task_id>/status/',
+         task_status_view, name='task_status'),
 ]
